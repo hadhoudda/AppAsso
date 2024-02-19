3c32bdecf2fe9717b1ad2481e8306c41d3eabb12
Houda Ben Abdallah <ahhouda@yahoo.fr>
2024-02-19 23:05:39 +0100

    added file html form

--- src/main/java/com/asso/frontend/controller/UserController.java
+++ src/main/java/com/asso/frontend/controller/UserController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.asso.frontend.model.User;
 import com.asso.frontend.service.UserService;
@@ -21,4 +25,37 @@ public class UserController {
 	    
 	    return "home";
 	}
+	
+	@GetMapping("/createUser")
+	public String createUser(Model model) {
+		User e = new User();
+		model.addAttribute("user", e);
+		return "formNewUser";
+	}
+	
+	@GetMapping("/updateUser/{id}")
+	public String updateUser(@PathVariable("id") final int id, Model model) {
+		User e = service.getUser(id);		
+		model.addAttribute("user", e);	
+		return "formUpdateUser";		
+	}
+	
+	@GetMapping("/deleteUser/{id}")
+	public ModelAndView deleteUser(@PathVariable("id") final int id) {
+		service.deleteUser(id);
+		return new ModelAndView("redirect:/");		
+	}
+	
+	@PostMapping("/saveUser")
+	public ModelAndView saveUser(@ModelAttribute User user) {
+		if(user.getId() != null) {
+			// User from update form has the password field not filled,
+			// so we fill it with the current password.
+			User current = service.getUser(user.getId());
+			user.setPassword(current.getPassword());
+		}
+		service.saveUser(user);
+		return new ModelAndView("redirect:/");	
+	}
+	
 }
--- src/main/java/com/asso/frontend/model/User.java
+++ src/main/java/com/asso/frontend/model/User.java
@@ -12,40 +12,5 @@ public class User {
 	 private Integer age;
 	 private String email;
 	 private String password;
-	public synchronized Integer getId() {
-		return id;
-	}
-	public synchronized void setId(Integer id) {
-		this.id = id;
-	}
-	public synchronized String getFirstName() {
-		return firstName;
-	}
-	public synchronized void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public synchronized String getLastName() {
-		return lastName;
-	}
-	public synchronized void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public synchronized Integer getAge() {
-		return age;
-	}
-	public synchronized void setAge(Integer age) {
-		this.age = age;
-	}
-	public synchronized String getEmail() {
-		return email;
-	}
-	public synchronized void setEmail(String email) {
-		this.email = email;
-	}
-	public synchronized String getPassword() {
-		return password;
-	}
-	public synchronized void setPassword(String password) {
-		this.password = password;
-	}
+	
 }
